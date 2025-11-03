82edc4bf2406b8eb3a8871e91978010c89506c44
nosemes <joan.masdemont@fasia.cat>
2025-11-03 10:34:41 +0100

    La lie parda

--- Primer_codi_telegram.py
+++ Primer_codi_telegram.py
@@ -7,26 +7,3 @@ from telebot import custom_filters
 bot = telebot.TeleBot("8398261892:AAEf-fgq_UVzA25Uxs8ZXJx-LVwrlBpjZoM") 
 
 
-@bot.message_handler(commands=['start'])
-def not_admin(message):
-    temperatura = random.randrange(20,30)
-    humitat = random.randrange(50,70)
-    bot.send_message(message.chat.id, f'T={temperatura} H={humitat}')
-    
-
-# Do not forget to register
-bot.add_custom_filter(custom_filters.ChatFilter())
-bot.polling(none_stop=True)
-
-
-
-'''
-Hem instalat telebot amb pip install telebot davant el dubte també fes pip3 install telebot
-'''
-
-
-'''
-
-modificacio despres d'haver-lo pujat una vegada
-
-'''
--- Segundo_Codigo_Telegram.py
+++ Segundo_Codigo_Telegram.py
@@ -8,40 +8,4 @@ import random
 bot = telebot.TeleBot('8398261892:AAEf-fgq_UVzA25Uxs8ZXJx-LVwrlBpjZoM') 
 
 
-@bot.message_handler(commands=['start'])
-
-def moviment(message):
-    
-    while True:
-        
-        numero = random.randrange(10,1000)
-        
-        if 0 < numero < 300:
-            
-             bot.send_message(message.chat.id, f"{numero} que pertenece a primer tercio!!!!")
-             
-        elif 300 <= numero < 666:
-            
-              bot.send_message(message.chat.id, f"{numero} que pertenece a segundo tercio!!!!")
-              
-        else:
-            
-              bot.send_message(message.chat.id, f"{numero} que pertenece a último tercio!!!!")
-              
-        time.sleep(10)
-
-
-bot.add_custom_filter(custom_filters.ChatFilter())
-
-bot.polling(none_stop=True)
-
-
-
-
-'''
-Modificació despres haverlo pujat una vegada
-
-'''
-
-
 
